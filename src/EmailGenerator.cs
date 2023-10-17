7f78d6999f8941620a7d2e362d60fbd758cf8b3c
Cavoq <Davidstromberger99@gmx.de>
2023-10-17 10:55:49 +0200

    Implement generateEmails

--- src/EmailGenerator.cs
+++ src/EmailGenerator.cs
@@ -1,24 +1,43 @@
 
+using System.Reflection.Metadata;
+
 namespace Egen
 {
     public class EmailGenerator
     {
-        private readonly string[] domains = { "gmail.com", "yahoo.com", "hotmail.com", "example.com" };
+        private const string defaultChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        private readonly string[] defaultDomains = { "gmail.com", "yahoo.com", "hotmail.com", "example.com" };
         private readonly Random random = new Random();
 
-        public string GenerateRandomEmail(uint length = 16, string? customDomain = null)
+        public string GenerateRandomEmail(uint length = 16, string? customDomain = null, string chars = defaultChars)
         {
-            string domain = customDomain ?? domains[random.Next(domains.Length)];
+            string domain = customDomain ?? defaultDomains[random.Next(defaultDomains.Length)];
 
-            string username = GenerateRandomString((int)length);
+            string username = GenerateRandomString((int)length, chars);
             string email = $"{username}@{domain}";
 
             return email;
         }
 
-        private string GenerateRandomString(int length)
+        // TODO: Support different formats
+        public string[] GenerateEmails(string[]? domainList = null, string chars = defaultChars)
         {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            string[] domains = domainList ?? defaultDomains;
+            string[] emails = Array.Empty<string>();
+            
+            for (int i = 0; i < domains.Length; ++i) 
+            {
+                uint length = 16;
+                string randomDomain = domains[random.Next(defaultDomains.Length)];
+                
+                emails[i] = GenerateRandomEmail(length, randomDomain, chars); 
+            }
+
+            return emails;
+        }
+
+        private string GenerateRandomString(int length, string chars)
+        {
             return new string(Enumerable.Repeat(chars, length)
               .Select(s => s[random.Next(s.Length)]).ToArray());
         }
